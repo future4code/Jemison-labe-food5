2fe223486e96a72a57408b40e72c25f5a0dfe56c
sterx17 <disasxster@aol.com>
2022-09-28 18:10:13 -0300

    <build> [página de busca] tentativas de fazer o render condicional

--- labefood/src/Pages/search/Busca.js
+++ labefood/src/Pages/search/Busca.js
@@ -10,8 +10,21 @@ export const Busca = () => {
 
     const { states } = useContext(Contexts)
 
-    const mostraResultados = () => {
-        if (states.searchInput.searchResults.length === 0) {
+    const restaurantsRender = () => {
+        states.carregandoRestaurantes && (<p>Carregando Histórico de Pedidos</p>)
+        !states.carregandoRestaurantes && states.erroRestaurantes && (<p>Houve um erro ao carregar o histórico. Recarregue a página.</p>)
+        !states.carregandoRestaurante && states.restaurantes && states.restaurantes.restaurants && states.restaurantes.restaurants.filter((item) => {
+            return item.name.includes(states.searchInput.searchResults)
+        })
+            .map((restaurante, index) => {
+                return (
+                    <BuscaResultadoCard key={index} logo={restaurante.logoUrl} name={restaurante.name} deliveryTime={restaurante.deliveryTime} shipping={restaurante.shipping} />
+                )
+            })
+    }
+
+    const condicaoPagina = () => {
+        if (states.searchInput.searchResults === '') {
             return (
                 <Container>
                     <Box marginTop={'1rem'}>
@@ -20,30 +33,29 @@ export const Busca = () => {
                 </Container>
             )
         } else {
-            states.carregandoRestaurantes && (<p>Carregando Histórico de Pedidos</p>)
-            !states.carregandoRestaurantes && states.erroRestaurantes && (<p>Houve um erro ao carregar o histórico. Recarregue a página.</p>)
-            !states.carregandoRestaurante && states.restaurantes && states.restaurantes.restaurants && states.restaurantes.restaurants.map((restaurante, index) => {
-                return <BuscaResultadoCard key={index} logo={restaurante.logoUrl} name={restaurante.name} deliveryTime={restaurante.deliveryTime} shipping={restaurante.shipping} />
-            })
-        } 
+            restaurantsRender()
+        }
     }
 
     return (
         <div>
             <HeaderSearch />
             <BarraBusca />
-            {mostraResultados()}
 
-            
+            {condicaoPagina()}
+
         </div>
     )
 }
 
 /*
+<Container>
+    <Box marginTop={'1rem'}>
+        <Typography textAlign={'center'} width={'100%'} variant="subtitle1">Busque por nome de restaurante</Typography>
+    </Box>
+</Container>
+                        
+                        
+                        
 
- 
-
-.filter((item)=>{
-                return item.name.includes(states.searchInput.searchResults)
-              })
 */
